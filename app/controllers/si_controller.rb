8f156646ddb050c04d7ca331a954feb5ec527b35
Logan Gingerich <loganpgingerich@gmail.com>
2017-12-20 12:37:04 -0700

    API Complete

---
+++ app/controllers/si_controller.rb
@@ -0,0 +1,98 @@
+class SiController < ApplicationController
+  def si
+    input = params[:units]
+    if input.present?
+      new_unit = string_conversion(input)
+      m_factor = calculate(input)
+      render json: {unit_name: new_unit, multiplication_factor: m_factor}
+    else
+      render json: {Status: 404, Error: "Not a valid unit."}
+    end
+  end
+
+  def string_conversion(string)
+    new_string = string.split(%r{(\+|\(|\)|\-|\/|\*)})
+    new_string.each do |str|
+      case str
+      when "minute"
+        str.gsub!("minute", "s")
+      when "hour"
+        str.gsub!("hour", "s")
+      when "degree"
+        str.gsub!("degree", "rad")
+      when "min"
+        str.gsub!("min", "s")
+      when "h"
+        str.gsub!("h", "s")
+      when "d"
+        str.gsub!("d", "s")
+      when "day"
+        str.gsub!("day", "s")
+      when "°"
+        str.gsub!("°", "rad")
+      when "‘"
+        str.gsub!("‘", "rad")
+      when "second"
+        str.gsub!("second", "rad")
+      when "“"
+        str.gsub!("“", "rad")
+      when "hectare"
+        str.gsub!("hectare", "m^2")
+      when "ha"
+        str.gsub!("ha", "m^2")
+      when "litre"
+        str.gsub!("litre", "m^3")
+      when "L"
+        str.gsub!("L", "m^3")
+      when "tonne"
+        str.gsub!("tonne", "kg")
+      when "t"
+        str.gsub!("t", "kg")
+      end
+    end
+    return new_string.join
+  end
+
+  def calculate(input)
+    array = input.split(%r{(\+|\(|\)|\-|\/|\*)})
+    array.each do |item|
+      case item
+      when "minute"
+        item.gsub!("minute", "60")
+      when "hour"
+        item.gsub!("hour", "3600")
+      when "degree"
+        item.gsub!("degree", "#{Math::PI / 180}")
+      when "min"
+        item.gsub!("min", "60")
+      when "h"
+        item.gsub!("h", "3600")
+      when "d"
+        item.gsub!("d", "86400")
+      when "day"
+        item.gsub!("day", "86400")
+      when "°"
+        item.gsub!("°", "#{Math::PI / 180}")
+      when "‘"
+        item.gsub!("‘", "#{Math::PI / 10800}")
+      when "second"
+        item.gsub!("second", "#{Math::PI / 648000}")
+      when "“"
+        item.gsub!("“", "#{Math::PI / 648000}")
+      when "hectare"
+        item.gsub!("hectare", "10000")
+      when "ha"
+        item.gsub!("ha", "10000")
+      when "litre"
+        item.gsub!("litre", "0.001")
+      when "L"
+        item.gsub!("L", "0.001")
+      when "tonne"
+        item.gsub!("tonne", "1000")
+      when "t"
+        item.gsub!("t", "1000")
+      end
+    end
+    return eval(array.join).to_f.round(14)
+  end
+end
